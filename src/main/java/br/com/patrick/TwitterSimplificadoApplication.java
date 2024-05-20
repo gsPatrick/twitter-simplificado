b07c5d3ea83a0c7a2f7520c4bd718c6a4f8894f5
Patrick Siqueira <118585848+gsPatrick@users.noreply.github.com>
2024-05-19 21:28:47 -0300

    Configurações iniciais do projeto

---
+++ src/main/java/br/com/patrick/TwitterSimplificadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.patrick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterSimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterSimplificadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/patrick/TwitterSimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.patrick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterSimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
