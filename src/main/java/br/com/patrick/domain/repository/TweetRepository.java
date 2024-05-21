5fd55172a5c05293668af01203fcf1e1a08ae4e5
Patrick <patrickgsiqueira@hotmail.com>
2024-05-21 18:33:36 -0300

    Atualização da classe repository: TweetRepository

--- src/main/java/br/com/patrick/domain/repository/TweetRepository.java
+++ src/main/java/br/com/patrick/domain/repository/TweetRepository.java
@@ -1,7 +1,12 @@
 package br.com.patrick.domain.repository;
 
 import br.com.patrick.domain.model.Tweet;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
+
+
 }
